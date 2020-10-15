7e906dc3dc5cb6b63c80d600ab99606c27755254
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-10-15 19:16:34 +0000

    hello

---
+++ hello.js
@@ -0,0 +1,14 @@
+
+const sayHello = function(name) {
+    console.log("Hello, " + name);
+  }
+  sayHello("Caliban");
+  sayHello("Miranda");
+  sayHello("Ferdinand");
+
+  const isEven  = function (num) {
+    return num % 2 === 0;
+  }
+  
+  console.log(isEven(10));
+  console.log(isEven(11));
